344feac9dec0bd7dcf6f4944061062e4d1ad4b57
NikoBK <112914618+NikoBK@users.noreply.github.com>
2023-11-16 21:47:41 +0100

    create Form1

--- VideoEnhancer/Form1.Designer.cs
+++ VideoEnhancer/Form1.Designer.cs
@@ -28,12 +28,60 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            videoPlayerImageBox = new PictureBox();
+            browseButton = new Button();
+            playButton = new Button();
+            ((System.ComponentModel.ISupportInitialize)videoPlayerImageBox).BeginInit();
+            SuspendLayout();
+            // 
+            // videoPlayerImageBox
+            // 
+            videoPlayerImageBox.BackColor = SystemColors.ActiveCaptionText;
+            videoPlayerImageBox.Location = new Point(95, 12);
+            videoPlayerImageBox.Name = "videoPlayerImageBox";
+            videoPlayerImageBox.Size = new Size(537, 353);
+            videoPlayerImageBox.SizeMode = PictureBoxSizeMode.Zoom;
+            videoPlayerImageBox.TabIndex = 0;
+            videoPlayerImageBox.TabStop = false;
+            // 
+            // browseButton
+            // 
+            browseButton.Location = new Point(104, 395);
+            browseButton.Name = "browseButton";
+            browseButton.Size = new Size(75, 23);
+            browseButton.TabIndex = 1;
+            browseButton.Text = "Browse";
+            browseButton.UseVisualStyleBackColor = true;
+            browseButton.Click += browseButton_Click;
+            // 
+            // playButton
+            // 
+            playButton.Location = new Point(186, 394);
+            playButton.Name = "playButton";
+            playButton.Size = new Size(75, 23);
+            playButton.TabIndex = 3;
+            playButton.Text = "Play";
+            playButton.UseVisualStyleBackColor = true;
+            playButton.Click += playButton_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(playButton);
+            Controls.Add(browseButton);
+            Controls.Add(videoPlayerImageBox);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)videoPlayerImageBox).EndInit();
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private PictureBox videoPlayerImageBox;
+        private Button browseButton;
+        private Button playButton;
     }
 }
--- VideoEnhancer/Form1.cs
+++ VideoEnhancer/Form1.cs
@@ -1,10 +1,37 @@
+using Emgu.CV;
+using Emgu.CV.CvEnum;
+
 namespace VideoEnhancer
 {
     public partial class Form1 : Form
     {
+        private VideoCapture _videoCapture;
+
         public Form1()
         {
             InitializeComponent();
         }
+
+        
+
+        private void browseButton_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "Video Files (*.mp4, *.avi,  *.flv)| *.mp4;*.avi*.flv";
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                Mat frame = new Mat();
+                _videoCapture = new VideoCapture(ofd.FileName);
+                {
+                    videoPlayerImageBox.Image = frame.ToBitmap();
+                    frame.Dispose();
+                }
+            }
+        }
+
+        private void playButton_Click(object sender, EventArgs e)
+        {
+            // TODO: Implement something here..
+        }     
     }
 }
