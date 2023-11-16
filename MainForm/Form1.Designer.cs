186f427dcb929d9f6db048f72d70b70cc65d27c1
NikoBK <112914618+NikoBK@users.noreply.github.com>
2023-11-16 16:43:00 +0100

    video playback
    This implementation is very buggy.

--- MainForm/Form1.Designer.cs
+++ MainForm/Form1.Designer.cs
@@ -28,38 +28,48 @@
         /// </summary>
         private void InitializeComponent()
         {
-            button1 = new Button();
-            button2 = new Button();
-            button3 = new Button();
+            playButton = new Button();
+            pauseButton = new Button();
+            stopButton = new Button();
             browseButton = new Button();
+            splitContainer1 = new SplitContainer();
+            videoPlayerImageBox = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)splitContainer1).BeginInit();
+            splitContainer1.Panel1.SuspendLayout();
+            splitContainer1.Panel2.SuspendLayout();
+            splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)videoPlayerImageBox).BeginInit();
             SuspendLayout();
             // 
-            // button1
+            // playButton
             // 
-            button1.Location = new Point(246, 335);
-            button1.Name = "button1";
-            button1.Size = new Size(95, 54);
-            button1.TabIndex = 0;
-            button1.Text = "play";
-            button1.UseVisualStyleBackColor = true;
+            playButton.Location = new Point(57, 3);
+            playButton.Name = "playButton";
+            playButton.Size = new Size(95, 54);
+            playButton.TabIndex = 0;
+            playButton.Text = "play";
+            playButton.UseVisualStyleBackColor = true;
+            playButton.Click += playButton_Click;
             // 
-            // button2
+            // pauseButton
             // 
-            button2.Location = new Point(373, 341);
-            button2.Name = "button2";
-            button2.Size = new Size(102, 48);
-            button2.TabIndex = 1;
-            button2.Text = "pause";
-            button2.UseVisualStyleBackColor = true;
+            pauseButton.Location = new Point(245, 9);
+            pauseButton.Name = "pauseButton";
+            pauseButton.Size = new Size(102, 48);
+            pauseButton.TabIndex = 1;
+            pauseButton.Text = "pause";
+            pauseButton.UseVisualStyleBackColor = true;
+            pauseButton.Click += pauseButton_Click;
             // 
-            // button3
+            // stopButton
             // 
-            button3.Location = new Point(502, 351);
-            button3.Name = "button3";
-            button3.Size = new Size(75, 23);
-            button3.TabIndex = 2;
-            button3.Text = "stop";
-            button3.UseVisualStyleBackColor = true;
+            stopButton.Location = new Point(426, 22);
+            stopButton.Name = "stopButton";
+            stopButton.Size = new Size(75, 23);
+            stopButton.TabIndex = 2;
+            stopButton.Text = "stop";
+            stopButton.UseVisualStyleBackColor = true;
+            stopButton.Click += stopButton_Click;
             // 
             // browseButton
             // 
@@ -71,25 +81,60 @@
             browseButton.UseVisualStyleBackColor = true;
             browseButton.Click += browseButton_Click;
             // 
+            // splitContainer1
+            // 
+            splitContainer1.Location = new Point(134, 12);
+            splitContainer1.Name = "splitContainer1";
+            splitContainer1.Orientation = Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            splitContainer1.Panel1.Controls.Add(videoPlayerImageBox);
+            // 
+            // splitContainer1.Panel2
+            // 
+            splitContainer1.Panel2.Controls.Add(stopButton);
+            splitContainer1.Panel2.Controls.Add(playButton);
+            splitContainer1.Panel2.Controls.Add(pauseButton);
+            splitContainer1.Panel2.Paint += splitContainer1_Panel2_Paint;
+            splitContainer1.Size = new Size(592, 426);
+            splitContainer1.SplitterDistance = 343;
+            splitContainer1.TabIndex = 4;
+            // 
+            // videoPlayerImageBox
+            // 
+            videoPlayerImageBox.BackColor = SystemColors.ActiveCaptionText;
+            videoPlayerImageBox.Location = new Point(0, 0);
+            videoPlayerImageBox.Name = "videoPlayerImageBox";
+            videoPlayerImageBox.Size = new Size(592, 340);
+            videoPlayerImageBox.SizeMode = PictureBoxSizeMode.StretchImage;
+            videoPlayerImageBox.TabIndex = 0;
+            videoPlayerImageBox.TabStop = false;
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(splitContainer1);
             Controls.Add(browseButton);
-            Controls.Add(button3);
-            Controls.Add(button2);
-            Controls.Add(button1);
             Name = "Form1";
             Text = "Under Water Drone Project";
+            splitContainer1.Panel1.ResumeLayout(false);
+            splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)splitContainer1).EndInit();
+            splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)videoPlayerImageBox).EndInit();
             ResumeLayout(false);
         }
 
         #endregion
 
-        private Button button1;
-        private Button button2;
-        private Button button3;
+        private Button playButton;
+        private Button pauseButton;
+        private Button stopButton;
         private Button browseButton;
+        private SplitContainer splitContainer1;
+        private PictureBox videoPlayerImageBox;
     }
 }
