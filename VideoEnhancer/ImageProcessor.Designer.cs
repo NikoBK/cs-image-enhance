5d879f98661d10f05e2394dcc91fdc009aa3a0bc
NikoBK <112914618+NikoBK@users.noreply.github.com>
2023-12-14 20:37:13 +0100

    update

--- VideoEnhancer/ImageProcessor.Designer.cs
+++ VideoEnhancer/ImageProcessor.Designer.cs
@@ -33,6 +33,13 @@
             saveButton = new Button();
             saveFileDialog1 = new SaveFileDialog();
             liquidRescaleButton = new Button();
+            label1 = new Label();
+            splitChannelsButton = new Button();
+            channelOneButton = new Button();
+            channelTwoButton = new Button();
+            channelThreeButton = new Button();
+            colorSpaceComboBox = new ComboBox();
+            colorCancelButton = new Button();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
             // 
@@ -78,11 +85,87 @@
             liquidRescaleButton.UseVisualStyleBackColor = true;
             liquidRescaleButton.Click += liquidRescaleButton_Click;
             // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 9F, FontStyle.Italic, GraphicsUnit.Point);
+            label1.Location = new Point(93, 543);
+            label1.Name = "label1";
+            label1.Size = new Size(172, 15);
+            label1.TabIndex = 6;
+            label1.Text = "<- This is really broken :keksad:";
+            // 
+            // splitChannelsButton
+            // 
+            splitChannelsButton.Location = new Point(538, 525);
+            splitChannelsButton.Name = "splitChannelsButton";
+            splitChannelsButton.Size = new Size(75, 51);
+            splitChannelsButton.TabIndex = 7;
+            splitChannelsButton.Text = "Split Channels";
+            splitChannelsButton.UseVisualStyleBackColor = true;
+            // 
+            // channelOneButton
+            // 
+            channelOneButton.Location = new Point(649, 525);
+            channelOneButton.Name = "channelOneButton";
+            channelOneButton.Size = new Size(75, 23);
+            channelOneButton.TabIndex = 8;
+            channelOneButton.Text = "Channel 1";
+            channelOneButton.UseVisualStyleBackColor = true;
+            channelOneButton.Click += channelOneButton_Click;
+            // 
+            // channelTwoButton
+            // 
+            channelTwoButton.Location = new Point(730, 525);
+            channelTwoButton.Name = "channelTwoButton";
+            channelTwoButton.Size = new Size(75, 23);
+            channelTwoButton.TabIndex = 9;
+            channelTwoButton.Text = "Channel 2";
+            channelTwoButton.UseVisualStyleBackColor = true;
+            channelTwoButton.Click += channelTwoButton_Click;
+            // 
+            // channelThreeButton
+            // 
+            channelThreeButton.Location = new Point(811, 525);
+            channelThreeButton.Name = "channelThreeButton";
+            channelThreeButton.Size = new Size(75, 23);
+            channelThreeButton.TabIndex = 10;
+            channelThreeButton.Text = "Channel 3";
+            channelThreeButton.UseVisualStyleBackColor = true;
+            channelThreeButton.Click += channelThreeButton_Click;
+            // 
+            // colorSpaceComboBox
+            // 
+            colorSpaceComboBox.FormattingEnabled = true;
+            colorSpaceComboBox.Items.AddRange(new object[] { "BGR", "RGB", "HSV", "LAB", "GRAY" });
+            colorSpaceComboBox.Location = new Point(765, 554);
+            colorSpaceComboBox.Name = "colorSpaceComboBox";
+            colorSpaceComboBox.Size = new Size(121, 23);
+            colorSpaceComboBox.TabIndex = 11;
+            colorSpaceComboBox.SelectedIndexChanged += colorSpaceComboBox_SelectedIndexChanged;
+            // 
+            // colorCancelButton
+            // 
+            colorCancelButton.Location = new Point(649, 553);
+            colorCancelButton.Name = "colorCancelButton";
+            colorCancelButton.Size = new Size(110, 23);
+            colorCancelButton.TabIndex = 12;
+            colorCancelButton.Text = "Cancel";
+            colorCancelButton.UseVisualStyleBackColor = true;
+            colorCancelButton.Click += colorCancelButton_Click;
+            // 
             // ImageProcessor
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(935, 583);
+            Controls.Add(colorCancelButton);
+            Controls.Add(colorSpaceComboBox);
+            Controls.Add(channelThreeButton);
+            Controls.Add(channelTwoButton);
+            Controls.Add(channelOneButton);
+            Controls.Add(splitChannelsButton);
+            Controls.Add(label1);
             Controls.Add(liquidRescaleButton);
             Controls.Add(saveButton);
             Controls.Add(cancelButton);
@@ -91,6 +174,7 @@
             Text = "ImageProcessor";
             ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
@@ -100,5 +184,12 @@
         private Button saveButton;
         private SaveFileDialog saveFileDialog1;
         private Button liquidRescaleButton;
+        private Label label1;
+        private Button splitChannelsButton;
+        private Button channelOneButton;
+        private Button channelTwoButton;
+        private Button channelThreeButton;
+        private ComboBox colorSpaceComboBox;
+        private Button colorCancelButton;
     }
 }
--- VideoEnhancer/ImageProcessor.cs
+++ VideoEnhancer/ImageProcessor.cs
@@ -9,12 +9,19 @@ using System.Drawing.Imaging;
 using System.Security.Cryptography;
 using ImageMagick;
 using System.Text.RegularExpressions;
+using Emgu.CV.CvEnum;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.ToolBar;
 
 namespace VideoEnhancer
 {
     public partial class ImageProcessor : Form
     {
         public Image Image { get; private set; }
+        public Image ProcessedImage { get; private set; }
+
+        // Color space conversion
+        private bool _showChannel1, _showChannel2, _showChannel3;
+        private ColorConversion _colorConversion = ColorConversion.Bgr2Rgb;
 
         public ImageProcessor()
         {
@@ -40,11 +47,11 @@ namespace VideoEnhancer
         private void saveButton_Click(object sender, EventArgs e)
         {
             this.saveFileDialog1.FileName = "Screenshot";
-            this.saveFileDialog1.Filter = "Image Files (*.png, *.jpg, *.jpeg)| *.png;*.jpg*.jpeg";
+            this.saveFileDialog1.Filter = "Image Files (*.png, *.jpg, *.jpeg)| *.png;*.jpg;*.jpeg";
             if (this.saveFileDialog1.ShowDialog() == DialogResult.OK)
             {
                 pictureBox1.Image.Save(this.saveFileDialog1.FileName, ImageFormat.Jpeg);
-                Close();
+                // Close();
             }
         }
 
@@ -73,6 +80,123 @@ namespace VideoEnhancer
                 }
             }
 
+        }
+
+        private void channelOneButton_Click(object sender, EventArgs e)
+        {
+            channelOneButton.Enabled = false;
+            channelTwoButton.Enabled = true;
+            channelThreeButton.Enabled = true;
+            _showChannel1 = true;
+            _showChannel2 = false;
+            _showChannel3 = false;
+        }
+
+        private void channelTwoButton_Click(object sender, EventArgs e)
+        {
+            channelOneButton.Enabled = true;
+            channelTwoButton.Enabled = false;
+            channelThreeButton.Enabled = true;
+            _showChannel1 = false;
+            _showChannel2 = true;
+            _showChannel3 = false;
+        }
+
+        private void channelThreeButton_Click(object sender, EventArgs e)
+        {
+            channelOneButton.Enabled = true;
+            channelTwoButton.Enabled = true;
+            channelThreeButton.Enabled = false;
+            _showChannel1 = false;
+            _showChannel2 = false;
+            _showChannel3 = true;
+        }
+
+        private void colorSpaceComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // Reset the selected color channel
+            _showChannel1 = false;
+            _showChannel2 = false;
+            _showChannel3 = false;
+
+            var domain = colorSpaceComboBox.Text;
+            if (domain == "BGR")
+            {
+                UpdateChannels("Blue", "Green", "Red");
+                return;
+            }
+
+            var domains = Constants.ColorDomains;
+            if (domains.ContainsKey(domain))
+            {
+                switch (domain)
+                {
+                    case "RGB":
+                        _colorConversion = ColorConversion.Bgr2Rgb;
+                        break;
+                    case "HSV":
+                        _colorConversion = ColorConversion.Bgr2Hsv;
+                        break;
+                    case "LAB":
+                        _colorConversion = ColorConversion.Bgr2Lab;
+                        break;
+                    case "GRAY":
+                        _colorConversion = ColorConversion.Bgr2Gray;
+                        break;
+                }
+            }
+            if (_colorConversion == ColorConversion.Bgr2Gray)
+            {
+                UpdateChannels(domains[domain][0], "", "", true, false, false);
+                return;
+            }
+
+            UpdateChannels(domains[domain][0], domains[domain][1], domains[domain][2]);
+            // ProcessedImage = CvInvoke.CvtColor(Image, ProcessedImage, _colorConversion);
+        }
+
+        private void UpdateChannels(string channel1, string channel2, string channel3, bool btn1 = true, bool btn2 = true, bool btn3 = true)
+        {
+            channelOneButton.Text = channel1;
+            channelTwoButton.Text = channel2;
+            channelThreeButton.Text = channel3;
+
+            channelOneButton.Enabled = btn1;
+            channelOneButton.Visible = btn1;
+            channelTwoButton.Enabled = btn2;
+            channelTwoButton.Visible = btn2;
+            channelThreeButton.Enabled = btn3;
+            channelThreeButton.Visible = btn3;
+        }
+
+        private void colorCancelButton_Click(object sender, EventArgs e)
+        {
+            HideChannelButtons();
+            _showChannel1 = _showChannel2 = _showChannel3 = false;
+            splitChannelsButton.Enabled = true;
+            pictureBox1.Image = Image;
+        }
+
+        private void HideChannelButtons()
+        {
+            channelOneButton.Text = "";
+            channelOneButton.Enabled = false;
+            channelOneButton.Visible = false;
+
+            channelTwoButton.Text = "";
+            channelTwoButton.Enabled = false;
+            channelTwoButton.Visible = false;
+
+            channelThreeButton.Text = "";
+            channelThreeButton.Enabled = false;
+            channelThreeButton.Visible = false;
+
+            colorCancelButton.Enabled = false;
+            colorCancelButton.Visible = false;
+
+            colorSpaceComboBox.Text = "";
+            colorSpaceComboBox.Enabled = false;
+            colorSpaceComboBox.Visible = false;
         }
     }
 }
